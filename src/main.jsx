9387f8af80bc0067c6799c83a95097a87b447d23
Charles042 <ekechukwuma20@gmail.com>
2024-06-03 21:08:21 +0100

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,25 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      {/* <ToastContainer
-        position="bottom-left"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        className="toast-container"
-        toastClassName={({ type }) =>
-          type === "success"
-            ? "toast-success"
-            : type === "info"
-            ? "toast-info"
-            : "toast-error"
-        }
-      /> */}
     </Provider>
   </React.StrictMode>
 );
